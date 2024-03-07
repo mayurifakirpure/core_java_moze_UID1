b6bf7080ec3a43b333edad1f614263743f949233
mayuri-hostnextra <mayuri@hostnextra.com>
2024-03-07 11:28:07 +0530

    Final Commit

---
+++ session_01/src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+	
+	static int a;
+	 static boolean b;
+	
+	public static void main(String args[])
+	{
+		System.out.println("Default value of int = "+a);
+		System.out.println("Default value of boolean = "+b);
+	}
+
+}
