b4fc07a826adfa5b30502f18136555d1977c2306
mayuri-hostnextra <mayuri@hostnextra.com>
2024-03-07 11:36:39 +0530

    Final Commit

---
+++ session_02/src/Simple.java
@@ -0,0 +1,38 @@
+
+public class Simple {
+	
+	public static void main(String args[])
+	{
+		System.out.print("Hello World!!");
+		
+	}
+
+}
+
+/*class: is keyword used to declare a class in java.
+
+public - access-modifier - visibility
+
+static - is keyword
+		static method
+		Memory Management- 
+		Exceuted by JVM - thus object creation not reqd
+
+void - does not return anything
+		
+String []args - Command line Arg
+
+sysout - used to print statement on console
+
+*	Object Creation:
+*
+*	new keyword:
+*	is used to allocate memory at runtime.
+*	All Objects get memory in Heap area at run-time
+*
+*
+*/
+
+
+
+
